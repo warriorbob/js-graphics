c6583be708b43acbc14929abd53f48730bc28fe5
warriorbob <mailinglists@thefighting.org>
2014-04-15 16:11:58 -0700

    Mucked about a little more and got a messed-up looking box texture from a tutorial that I need to figure out how to un-mess-up.

--- threejsdemo/js/script.js
+++ threejsdemo/js/script.js
@@ -1,21 +1,23 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.CanvasRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement);
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshNormalMaterial( { color: 0x33ff55 } );
+var geometry = new THREE.BoxGeometry(100,100,100);
+var material = new THREE.MeshLambertMaterial({
+	map: THREE.ImageUtils.loadTexture('http://www.html5canvastutorials.com/demos/assets/crate.jpg')
+});
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 2;
+camera.position.z = 200;
 
 function render() {
 	requestAnimationFrame(render);
-	cube.rotation.x += 0.04;
-	cube.rotation.y += 0.02;
+	cube.rotation.x -= 0.01;
+	cube.rotation.y -= 0.005;
 	renderer.render(scene, camera);
 }
 render();
