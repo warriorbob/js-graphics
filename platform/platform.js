d993e31a29fea97356201954547befdfb962f67c
warriorbob <mailinglists@thefighting.org>
2015-03-02 12:32:08 -0800

    Added some things that I apparently did?

--- platform/platform.js
+++ platform/platform.js
@@ -17,27 +17,82 @@ Boing boing boing.
 
 const BOXSIZE = 16;
 
+var dude = {
+	xpos: 100,
+	ypos: 100
+}
+
+var sprite = new Image();
+sprite.src = 'dude.bmp';
+
 var drawGrid = function()
 {
 	for(var i = 0; i < canvas.height / BOXSIZE; i++)
-	{
+	{	//Horizontal lines
 		ctx.beginPath();
-		ctx.moveTo(0, i * BOXSIZE);
+		ctx.moveTo(0.5, i * BOXSIZE);
 		ctx.lineTo(canvas.width, i * BOXSIZE);
-		ctx.lineWidth = "1";
+		ctx.lineWidth = "0.2";
 		ctx.stroke();
 	}
 
 	for(var j = 0; j < canvas.width / BOXSIZE; j++)
 	{
+		//Vertical lines
 		ctx.beginPath();
-		ctx.moveTo(j * BOXSIZE, 0);
+		ctx.moveTo(j * BOXSIZE, 0.5);
 		ctx.lineTo(j * BOXSIZE, canvas.height);
-		ctx.lineWidth = "1";
+		ctx.lineWidth = "0.2";
 		ctx.stroke();
 	}
 }
 
+var drawDude = function(x, y)
+{
+	ctx.drawImage(sprite, x, y);
+}
 
-// GO DO STUFF
-drawGrid();
+// GO DO STUFF 
+var draw = function()
+{
+	drawGrid();
+	drawDude(dude.xpos, dude.ypos);
+}
+
+var update = function()
+{
+	dude.ypos++;
+};
+
+var resetGraphics = function(modifier) {
+   ctx.save();
+   // Use the identity matrix while clearing the canvas
+   ctx.setTransform(1, 0, 0, 1, 0, 0);
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   ctx.restore();
+
+   ctx.beginPath();
+	ctx.moveTo(0,0);
+	ctx.lineTo(canvas.width,0);
+	ctx.lineTo(canvas.width,canvas.height);
+	ctx.lineTo(0,canvas.height);
+	ctx.lineTo(0,0);
+	ctx.stroke();
+};
+
+//---------------
+// Go
+//---------------
+var main = function() {
+	var now = Date.now();
+	var delta = now - then;
+	resetGraphics();
+	update(delta / 1000);
+	draw();
+	then = now;
+};
+
+//off we go
+var ready = true;
+var then = Date.now();
+setInterval(main,1);
--- threejsdemo/js/script.js
+++ threejsdemo/js/script.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.CanvasRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(100,100,100);
